70b4435b247c6b46dc9a3ff232fbee3af507aab0
YoungProducer <sashabezrukovownmail@gmail.com>
2021-02-06 21:30:33 +0200

    improved scan-dir function.

--- src/lib/scan-dir.cpp
+++ src/lib/scan-dir.cpp
@@ -1,19 +1,44 @@
 #include "scan-dir.h"
 
-FilesMap scan_dir(std::string path)
+std::vector<std::string> get_dir_content(std::string path)
 {
-    FilesMap result;
+    std::vector<std::string> result;
 
     for (const auto &file : std::filesystem::directory_iterator(path))
     {
-        std::string filePath = file.path().string();
+        result.push_back(file.path().string());
+    }
 
-        std::string checksum = SHA256_CHECKSUM(filePath.c_str());
+    return result;
+}
 
-        /** doesn't include "path" which passes from params */
-        std::string relativePath = filePath.substr(filePath.find(path) + path.size());
+void get_dir_content(std::string path, std::vector<std::string> *content)
+{
+    for (const auto &file : std::filesystem::directory_iterator(path))
+    {
+        content->push_back(file.path().string());
+    }
+}
 
-        result.insert(std::make_pair(relativePath, checksum));
+FilesMap scan_dir(std::string path)
+{
+    FilesMap result;
+    std::vector<std::string> dir_content;
+
+    if (std::filesystem::is_directory(path))
+    {
+        get_dir_content(path, &dir_content);
+
+        for (int i = 0, length = dir_content.size(); i < length; i++)
+        {
+            std::string curr_path = dir_content[i];
+            std::string checksum = SHA256_CHECKSUM(path.c_str());
+
+            /** doesn't include "path" which passes from params */
+            std::string relative_path = curr_path.substr(curr_path.find(path) + path.size());
+
+            result.insert(std::make_pair(relative_path, checksum));
+        }
     }
 
     return result;
--- src/lib/scan-dir.h
+++ src/lib/scan-dir.h
@@ -1,11 +1,15 @@
 #ifndef SCAN_DIR
 #define SCAN_DIR
 
+#include <vector>
 #include <filesystem>
-#include <boost/unordered_map.hpp>
+#include <boost/range.hpp>
 
 #include "../types/simple.h"
 #include "sha256-checksum.h"
+
+std::vector<std::string> get_dir_content(std::string path);
+void get_dir_content(std::string path, std::vector<std::string> *content);
 
 FilesMap scan_dir(std::string path);
 
