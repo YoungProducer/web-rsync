f44f1cc8c66c3f63f65f0b591aaede0b24e1aba3
YoungProducer <sashabezrukovownmail@gmail.com>
2021-02-07 13:52:32 +0200

    removed useless code.

--- src/index.cpp
+++ src/index.cpp
@@ -32,39 +32,15 @@ Napi::Array test(const Napi::CallbackInfo &info)
     std::vector<FileAction *> diff = get_folders_diff(prev, &next);
     auto stop = std::chrono::high_resolution_clock::now();
 
-    // for (const auto &e : result)
-    // {
-    //     std::cout << "path: " << e.first << std::endl
-    //               << "checksum: " << e.second << std::endl
-    //               << std::endl;
-    // }
-
-    // for (const auto &e : diff)
-    // {
-    //     std::cout << "path: " << e->path << std::endl
-    //               << "action: " << e->type << std::endl
-    //               << std::endl;
-    // }
-
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
     std::cout << "Time taken by function: "
               << duration.count() << " microseconds" << std::endl;
 
-    Napi::Array arr = Napi::Array::New(env);
-
     auto startCoerce = std::chrono::high_resolution_clock::now();
-    for (int i = 0, length = diff.size(); i < length; i++)
-    {
-        Napi::Object value = Napi::Object::New(env);
-        value.Set(
-            Napi::String::New(env, "path"),
-            Napi::String::New(env, diff[i]->path.c_str()));
-        value.Set(
-            Napi::String::New(env, "type"),
-            Napi::String::New(env, diff[i]->type.c_str()));
-        arr.Set(i, value);
-    }
+
+    Napi::Array result = TypeTransform::Advanced::vector_to_napi_array<FileAction *>(env, diff, TypeTransform::Embedded::file_action_to_napi_object);
+
     auto stopCoerce = std::chrono::high_resolution_clock::now();
 
     auto durationCoerce = std::chrono::duration_cast<std::chrono::microseconds>(stopCoerce - startCoerce);
@@ -72,9 +48,6 @@ Napi::Array test(const Napi::CallbackInfo &info)
     std::cout << "Time taken by function: "
               << durationCoerce.count() << " microseconds" << std::endl;
 
-    Napi::Array result = TypeTransform::Advanced::vector_to_napi_array<FileAction *>(env, diff, TypeTransform::Embedded::file_action_to_napi_object);
-
-    // return Napi::String::New(env, "good");
     return result;
 }
 
