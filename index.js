1185a5e904aad4714fda332c834d17f6bdb446c1
YoungProducer <sashabezrukovownmail@gmail.com>
2021-02-08 15:46:13 +0200

    added server and client initializators.

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-const { test, scanDir, getFoldersDiff } = require('./build/Release/file-checksum.node');
+const native = require('./build/Release/file-checksum.node');
+const lib = require('./lib');
 
-// console.log(fileChecksum.sha256_checksum('./test-files/image.png'))
-// console.log(fileChecksum.test('../history-intro/dist', '../biology-intro/dist'))
-// fileChecksum.test('../history-intro/dist', '../biology-intro/dist')
-
-console.time()
-// const prev = scanDir('./test-files/prev');
-const next = scanDir('../biology-intro/dist');
-
-// console.log(prev, next)
-
-const diff = getFoldersDiff('../biology-intro/dist', next);
-
-console.log(diff);
-console.timeEnd()
+module.exports = { ...native, ...lib };
